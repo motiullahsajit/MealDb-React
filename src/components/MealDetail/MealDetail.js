256c19bf12dbf9c7686b3493788f271b7622ca63
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-03-13 00:32:02 +0600

    done

--- src/components/MealDetail/MealDetail.js
+++ src/components/MealDetail/MealDetail.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
 const MealDetail = () => {
-    const [meal, setMeal] = useState({});
-    useEffect(() => {
-        const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setMeal(data?.meals?.[0]))
-    }, [])
+    // const [meal, setMeal] = useState({});
+    // useEffect(() => {
+    //     const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => setMeal(data?.meals?.[0]))
+    // }, [])
     return (
         <div>
             {/* <h1>This is meal detail</h1> */}
